17a9eb5872acba5a9867cc98371d5d97c1abb432
Sandeep Chandragiri <71395305+SandeepChandragiri3@users.noreply.github.com>
2021-07-31 15:23:50 +0530

    Update cal.go

--- cal.go
+++ cal.go
@@ -1,23 +1,68 @@
-func add(int number1, int number2) int{
-    
-   return number1 + number2;
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func parseArgs(c []string) (float64, float64, error) {
+	num1, err := strconv.ParseFloat(c[0], 64)
+	if err != nil {
+		return 0.0, 0.0, err
+	}
+	num2, err := strconv.ParseFloat(c[2], 64)
+	if err != nil {
+		return 0.0, 0.0, err
+	}
+	return num1, num2, nil
 }
 
-func multiply(int number1, int number2) int{
-    
-   return number1 * number2;
+func processStack(e []string) (float64, error) {
+	result := 0.0
+	for _, v := range e {
+		c := strings.Split(v, " ")
+		if len(c)-1 < 2 {
+			return 0.0, errors.New("error: some arguments are not supplied")
+		}
+		num1, num2, err := parseArgs(c)
+		if err != nil {
+			return 0.0, err
+		}
+		switch c[1] {
+		case "*":
+			result = num1 * num2
+		case "/":
+			if num2 == 0.0 {
+				return 0.0, errors.New("error: you tried to divide by zero.")
+			}
+			result = num1 / num2
+		case "+":
+			result = num1 + num2
+		case "-":
+			result = num1 - num2
+		}
+	}
+	return result, nil
 }
 
-func divide(int number1, int number2) float{
-    
-    if number2 ==  0 {
-       panic("you can not divide to 0") //or replace panic with fmt.Println();
-    }
-
-   return number1 / number2;
-}
-
-func substract(int number1, int number2) int{
-    
-   return number1 - number2;
+func main() {
+	expressions := make([]string, 1)
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Print("gocalc>")
+		for scanner.Scan() {
+			expressions = append(expressions, scanner.Text())
+			res, err := processStack(expressions)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(res)
+			}
+			fmt.Print("gocalc>")
+		}
+	}
 }
